730f2036c31bcaccf8d6d784782debad271ff734
Fabricio817 <FABRICIOALBUQUERQUE817@GMAIL.COM>
2025-07-25 02:25:30 +0000

    super trunfo att caracteristica 2

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -3,11 +3,14 @@
 //area onde foram cadastrada todas as variaveis.
 
 int main() {
- //cadastro de vaiaveis
- int opção;
- int menu;
- int carta1, carta2;
- int Caracteristica;
+ 
+    //cadastro de vaiaveis
+    int opção;
+    int menu;
+    int carta1, carta2;
+    int caracteristica;
+    int caracteristica2;
+    int media, media2, media3;
     char nome_cidade1[50] = "Carta1-Manaus(AM)", nome_cidade2[50] = "Carta2-Belém(PA)";
     int populacao_cidade1 = 2279686, populacao_cidade2 = 1303403;
     float area_cidade1 = 11401, area_cidade2 = 1059;
@@ -15,7 +18,8 @@ int main() {
     int numeros_de_pontos_turisticos_cidade1 = 10, numeros_de_pontos_turisticos_cidade2 = 5;
     float DensidadePopulacional1, DensidadePopulacional2;
     float PIB_per_capita1, PIB_per_capita2;
-    int caracteristica;
+   
+
 
 //nessa area foi area destinada para o calculo de (densidade populacional & PIB percapita)
 
@@ -57,15 +61,15 @@ case 2:
     printf("O jogador com a carta que tiver o maior valor na característica escolhida na rodada leva as cartas\n");
     printf("dos outros jogadores, colocando-as atrás do seu monte. A carta Super Trunfo, se houver, tem prioridade\n");
     printf("sobre todas as outras, exceto cartas com a letra A,e o jogo termina quando um jogador acumula todas as cartas.zn");
- break;
+
+    break;
+
 case 3:
     printf("###Ate a proxima!####");
- break;
 
-default:
-printf("OPção invalida\n Por favor digitar uma opção valida!\n");
+    break;
 
-break;
+
 }
 // switch de apresentação da carta escolhida
 switch (carta1)
@@ -80,9 +84,14 @@ case 1:
     printf("6.  PIB per capita: R$ 19.200\n");
    
     printf("###Escolha qual caracterisca gostaria de jogar:### \n"); 
-    scanf("%d", &Caracteristica);
+    scanf("%d", &caracteristica);
+
+    printf("###Escolha qual caracterisca gostaria de jogar:### \n"); 
+    scanf("%d", &caracteristica2);
+   
 
     break;
+
     case 2:
     printf("Você escolheu a carta 2: Belém(PA)\n");
     printf("1. População: 1303403\n");
@@ -91,33 +100,42 @@ case 1:
     printf("4. Número de pontos turísticos: 5\n");
     printf("5. Densidade populacional: 1200 hab/km²\n");
     printf("6. PIB per capita: R$ 49000\n");
+    
 
-     printf("###Escolha qual caracterisca gostaria de jogar:### \n"); 
-    scanf("%d", &Caracteristica);
+    printf("###Escolha duas Caracteristicas### \n");
+    scanf("%d \n", &caracteristica);
 
+    printf("###Escolha segunda caracteristica###");
+    scanf("%d \n", &caracteristica2);
 
     break;
-default:
+  
+    default:
     printf("Carta inválida. Por favor, escolha uma carta válida.\n");
     break;
 }
-
-
-    switch (Caracteristica)
-{
-case 1:   
+   
+switch (caracteristica)
+   {
+   
+ case 1:   
     if (populacao_cidade1 > populacao_cidade2) {
         printf("Cidade Manaus(AM) X Cidade Belem(PA).\n");
         printf("o Atributo comprado foi População.\n");
         printf("População Manaus: %d de Pessoas\n", populacao_cidade1);
         printf("População Belém: %d de Pessoas\n", populacao_cidade2);
         printf("Manaus(AM) Venceu!\n");
+
+
+   
     } else if (populacao_cidade1 < populacao_cidade2) {
        printf("Cidade Manaus(AM) X Cidade Belem(PA).\n");
         printf("o Atributo comprado foi População.\n");
         printf("População Manaus: %d de Pessoas\n", populacao_cidade1);
         printf("População Belém: %d de Pessoas\n", populacao_cidade2);
         printf("Belém(PA) Venceu!\n");
+
+   
     } else {
         printf("Manaus(AM) e Belém(PA) têm a mesma população.\n");
         printf("o Atributo comprado foi População.\n");
@@ -223,10 +241,147 @@ case 4:
         printf("A disputa Acabou em Empate!\n");
     }
     break;
+   
     default:
-    printf("Essa nao é uma opção valida!");
+    printf("Essa nao é uma opção valida!\n");
     printf("Por favor escolha uma opção valida!");
 
+    return caracteristica;
 }
-return 0;
+switch (caracteristica2)
+   {
+   
+case 1:   
+    if (populacao_cidade1 > populacao_cidade2) {
+        printf("Cidade Manaus(AM) X Cidade Belem(PA).\n");
+        printf("o Atributo comprado foi População.\n");
+        printf("População Manaus: %d de Pessoas\n", populacao_cidade1);
+        printf("População Belém: %d de Pessoas\n", populacao_cidade2);
+        printf("Manaus(AM) Venceu!\n");
+
+
+   
+    } else if (populacao_cidade1 < populacao_cidade2) {
+       printf("Cidade Manaus(AM) X Cidade Belem(PA).\n");
+        printf("o Atributo comprado foi População.\n");
+        printf("População Manaus: %d de Pessoas\n", populacao_cidade1);
+        printf("População Belém: %d de Pessoas\n", populacao_cidade2);
+        printf("Belém(PA) Venceu!\n");
+
+   
+    } else {
+        printf("Manaus(AM) e Belém(PA) têm a mesma população.\n");
+        printf("o Atributo comprado foi População.\n");
+        printf("A disputa Acabou em Empate!\n");
+    }
+    
+    break;
+case 2:
+    if (area_cidade1 > area_cidade2) {
+        printf("Cidade Manaus(AM) X Cidade Belem(PA).\n");
+        printf("o Atributo comprado foi Área.\n");
+        printf("Área Manaus: %.2f km²\n", area_cidade1);
+        printf("Área Belém: %.2f km²\n", area_cidade2);
+        printf("Manaus(AM) Venceu!\n");
+    } else if (area_cidade1 < area_cidade2) {
+        printf("Cidade Manaus(AM) X Cidade Belem(PA).\n");
+        printf("o Atributo comprado foi Área.\n");
+        printf("Área Manaus: %.2f km²\n", area_cidade1);
+        printf("Área Belém: %.2f km²\n", area_cidade2);
+        printf("Belém(PA) Venceu!\n");
+    } else {
+        printf("Manaus(AM) e Belém(PA) têm a mesma área.\n");
+        printf("o Atributo comprado foi Área.\n");
+        printf("A disputa Acabou em Empate!\n");
+    }
+   
+    break;
+case 3:
+    if (PIB_cidade1 > PIB_cidade2) {
+        printf("Cidade Manaus(AM) X Cidade Belem(PA).\n");
+        printf("o Atributo comprado foi PIB.\n");
+        printf("PIB Manaus: R$ %.2f\n", PIB_cidade1);
+        printf("PIB Belém: R$ %.2f\n", PIB_cidade2);
+        printf("Manaus(AM) Venceu!\n");
+    } else if (PIB_cidade1 < PIB_cidade2) {
+        printf("Cidade Manaus(AM) X Cidade Belem(PA).\n");
+        printf("o Atributo comprado foi PIB.\n");
+        printf("PIB Manaus: R$ %.2f\n", PIB_cidade1);
+        printf("PIB Belém: R$ %.2f\n", PIB_cidade2);
+        printf("Belém(PA) Venceu!\n");
+    } else {
+        printf("Manaus(AM) e Belém(PA) têm o mesmo PIB.\n");
+        printf("o Atributo comprado foi PIB.\n");
+        printf("A disputa Acabou em Empate!\n");
+    }
+    break;
+case 4:
+    if (numeros_de_pontos_turisticos_cidade1 > numeros_de_pontos_turisticos_cidade2) {
+        printf("Cidade Manaus(AM) X Cidade Belem(PA).\n");
+        printf("o Atributo comprado foi Número de pontos turísticos.\n");
+        printf("Número de pontos turísticos Manaus: %d\n", numeros_de_pontos_turisticos_cidade1);
+        printf("Número de pontos turísticos Belém: %d\n", numeros_de_pontos_turisticos_cidade2);
+        printf("Manaus(AM) Venceu!\n");
+    } else if (numeros_de_pontos_turisticos_cidade1 < numeros_de_pontos_turisticos_cidade2) {
+        printf("Cidade Manaus(AM) X Cidade Belem(PA).\n");
+        printf("o Atributo comprado foi Número de pontos turísticos.\n");
+        printf("Número de pontos turísticos Manaus: %d\n", numeros_de_pontos_turisticos_cidade1);
+        printf("Número de pontos turísticos Belém: %d\n", numeros_de_pontos_turisticos_cidade2);
+        printf("Belém(PA) Venceu!\n");
+    } else {
+        printf("Manaus(AM) e Belém(PA) têm o mesmo número de pontos turísticos.\n"); 
+        printf("o Atributo comprado foi Número de pontos turísticos.\n");
+        printf("A disputa Acabou em Empate!\n");
+    }
+        break;
+   case 5:
+    if (DensidadePopulacional1 > DensidadePopulacional2)
+    {
+        printf("Cidade Manaus(AM) X Cidade Belem(PA).\n");
+        printf("o Atributo comprado foi Densidade Populacional.\n");
+        printf("Densidade Populacional Manaus: %.2f hab/km²\n", DensidadePopulacional1);
+        printf("Densidade Populacional Belém: %.2f hab/km²\n", DensidadePopulacional2);
+        printf("Manaus(AM) Venceu!\n");
+    } else if (DensidadePopulacional1 < DensidadePopulacional2) {
+        printf("Cidade Manaus(AM) X Cidade Belem(PA).\n");
+        printf("o Atributo comprado foi Densidade Populacional.\n");
+        printf("Densidade Populacional Manaus: %.2f hab/km²\n", DensidadePopulacional1);
+        printf("Densidade Populacional Belém: %.2f hab/km²\n", DensidadePopulacional2);
+        printf("Belém(PA) Venceu!\n");
+    } else {
+        printf("Manaus(AM) e Belém(PA) têm a mesma densidade populacional.\n");
+        printf("o Atributo comprado foi Densidade Populacional.\n");
+        printf("A disputa Acabou em Empate!\n");
+    }
+
+    break;
+    case 6:
+    if (PIB_per_capita1 > PIB_per_capita2) {
+        printf("Cidade Manaus(AM) X Cidade Belem(PA).\n");
+        printf("o Atributo comprado foi PIB per capita.\n");
+        printf("PIB per capita Manaus: R$ %.2f\n", PIB_per_capita1);
+        printf("PIB per capita Belém: R$ %.2f\n", PIB_per_capita2);
+        printf("Manaus(AM) Venceu!\n");
+    } else if (PIB_per_capita1 < PIB_per_capita2) {
+        printf("Cidade Manaus(AM) X Cidade Belem(PA).\n");
+        printf("o Atributo comprado foi PIB per capita.\n");
+        printf("PIB per capita Manaus: R$ %.2f\n", PIB_per_capita1);
+        printf("PIB per capita Belém: R$ %.2f\n", PIB_per_capita2);
+        printf("Belém(PA) Venceu!\n");
+    } else {
+        printf("Manaus(AM) e Belém(PA) têm o mesmo PIB per capita.\n");
+        printf("o Atributo comprado foi PIB per capita.\n");
+        printf("A disputa Acabou em Empate!\n");
+    }
+    break;
+   
+    default:
+    printf("Essa nao é uma opção valida!\n");
+    printf("Por favor escolha uma opção valida!");
+
+ return caracteristica2;
 }
+
+
+}
+
